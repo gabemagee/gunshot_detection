66df7e9976800519f568f5ddd6191859ddc8dd09
laurenogd <51664503+laurenogd@users.noreply.github.com>
2019-06-12 13:49:46 -0400

    minor edits, added code to use GPU on volta

--- python/Lauren/gunshot_cnn_current_lauren.py
+++ python/Lauren/gunshot_cnn_current_lauren.py
@@ -3,7 +3,7 @@
 
 # # Library Imports
 
-# In[1]:
+# In[3]:
 
 
 # File Directory 
@@ -61,7 +61,7 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 
 # # Data
 
-# In[2]:
+# In[4]:
 
 
 samples=[]
@@ -78,130 +78,146 @@ samples = np.load("/home/lauogden/data/gunshot_sound_samples.npy")
 labels = np.load("/home/lauogden/data/gunshot_sound_labels.npy")
 
 
-# # Split Data
-
-# In[27]:
-
-
-#train test split
-
-kf = KFold(n_splits=3, shuffle=True)
-samples = np.array(samples)
-labels = np.array(labels)
-for train_index, test_index in kf.split(samples):
-    train_wav, test_wav = samples[train_index], samples[test_index]
-    train_label, test_label = labels[train_index], labels[test_index]
-
-
-# In[30]:
-
-
-train_wav = np.array(train_wav)
-test_wav = np.array(test_wav)
-
-#Reshape data
-train_wav = train_wav.reshape(-1,44100,1)
-test_wav = test_wav.reshape(-1,44100,1)
-train_label = keras.utils.to_categorical(train_label, 2)
-test_label = keras.utils.to_categorical(test_label, 2)
-
-
-# # Model
-
-# In[29]:
-
-
-# Parameters
-lr = 0.001
-generations = 20000
-num_gens_to_wait = 250
-batch_size = 32
-drop_out_rate = 0.2
-input_shape = (44100,1)
-
-
-# In[14]:
-
-
-input_tensor = Input(shape=input_shape)
-
-x = layers.Conv1D(8, 11, padding='valid', activation='relu', strides=1)(input_tensor)
-x = layers.MaxPooling1D(2)(x)
-x = layers.Conv1D(16, 7, padding='valid', activation='relu', strides=1)(x)
-x = layers.MaxPooling1D(4)(x)
-x = layers.Conv1D(32, 5, padding='valid', activation='relu', strides=1)(x)
-x = layers.MaxPooling1D(4)(x)
-x = layers.Conv1D(64, 5, padding='valid', activation='relu', strides=1)(x)
-x = layers.MaxPooling1D(6)(x)
-x = layers.Conv1D(128, 3, padding='valid', activation='relu', strides=1)(x)
-x = layers.MaxPooling1D(6)(x)
-x = layers.Conv1D(256, 3, padding='valid', activation='relu', strides=1)(x)
-x = layers.MaxPooling1D(6)(x)
-x = layers.Flatten()(x)
-x = layers.Dense(100, activation='relu')(x)
-x = layers.Dropout(drop_out_rate)(x)
-x = layers.Dense(50, activation='relu')(x)
-x = layers.Dropout(drop_out_rate)(x)
-x = layers.Dense(20, activation='relu')(x)
-output_tensor = layers.Dense(2, activation='softmax')(x)
-
-model = tf.keras.Model(input_tensor, output_tensor)
-
-model.compile(loss=keras.losses.categorical_crossentropy,
-             optimizer=keras.optimizers.Adam(lr = lr),
-             metrics=['accuracy'])
-
-
 # In[ ]:
 
 
-model_filename = '/home/lauogden/gunshot_cnn_model.pkl' 
-
-# add callbacks to stop early if it stops improving
-
-callbacks = [
-    EarlyStopping(monitor='val_acc',
-                  patience=10,
-                  verbose=1,
-                  mode='auto'),
-    
-    ModelCheckpoint(model_filename, monitor='val_acc',
-                    verbose=1,
-                    save_best_only=True,
-                    mode='auto'),
-]
+print("There are " + str(len(samples)) + " samples.")
+print("There are " + str(len(labels)) + " labels.")
 
 
-# In[15]:
+# # Split Data
+
+# In[6]:
 
 
-model.summary()
+#TEST IF GPU IS AVILABLE?
+print("TESTING IF GPU IS AVAILABLE:")
+print(tf.test.is_gpu_available())
 
 
-# In[16]:
+#USING THE GPU:
+
+with tf.device(“/gpu:0”):
+    #train test split
+
+    kf = KFold(n_splits=3, shuffle=True)
+    samples = np.array(samples)
+    labels = np.array(labels)
+    for train_index, test_index in kf.split(samples):
+        train_wav, test_wav = samples[train_index], samples[test_index]
+        train_label, test_label = labels[train_index], labels[test_index]
 
 
-model.load("/home/lauogden/gunshot_cnn_model.h5")
-
-#FIT IT
-model.fit(train_wav, train_label, 
-          validation_data=[test_wav, test_label],
-          epochs=50,
-          callbacks=model_callbacks,
-          verbose=1,
-          batch_size=batch_size,
-          shuffle=True)
-
-model.save("/home/lauogden/gunshot_cnn_model.h5")
+    # In[9]:
 
 
-# In[26]:
+    #Reshape data
+    train_wav = train_wav.reshape(-1,44100,1)
+    test_wav = test_wav.reshape(-1,44100,1)
+    train_label = keras.utils.to_categorical(train_label, 2)
+    test_label = keras.utils.to_categorical(test_label, 2)
 
 
-#incorrectly predicted data samples
-Y_test_pred = model.predict(test_wav)
-y_predicted_classes_test = y_test_pred.argmax(axis=-1)
-y_actual_classes_test= test_label.argmax(axis=-1)
-wrong_examples = np.nonzero(y_predicted_classes_test != y_actual_classes_test)
-print(wrong_examples)
+    # In[ ]:
+
+
+    print(train_wav.shape)
+
+
+    # # Model
+
+    # In[10]:
+
+
+    # Parameters
+    lr = 0.001
+    batch_size = 32
+    drop_out_rate = 0.2
+
+
+    # In[11]:
+
+
+    input_tensor = Input(shape=input_shape)
+
+    x = layers.Conv1D(8, 11, padding='valid', activation='relu', strides=1)(input_tensor)
+    x = layers.MaxPooling1D(2)(x)
+    x = layers.Conv1D(16, 7, padding='valid', activation='relu', strides=1)(x)
+    x = layers.MaxPooling1D(4)(x)
+    x = layers.Conv1D(32, 5, padding='valid', activation='relu', strides=1)(x)
+    x = layers.MaxPooling1D(4)(x)
+    x = layers.Conv1D(64, 5, padding='valid', activation='relu', strides=1)(x)
+    x = layers.MaxPooling1D(6)(x)
+    x = layers.Conv1D(128, 3, padding='valid', activation='relu', strides=1)(x)
+    x = layers.MaxPooling1D(6)(x)
+    x = layers.Conv1D(256, 3, padding='valid', activation='relu', strides=1)(x)
+    x = layers.MaxPooling1D(6)(x)
+    x = layers.Flatten()(x)
+    x = layers.Dense(100, activation='relu')(x)
+    x = layers.Dropout(drop_out_rate)(x)
+    x = layers.Dense(50, activation='relu')(x)
+    x = layers.Dropout(drop_out_rate)(x)
+    x = layers.Dense(20, activation='relu')(x)
+    output_tensor = layers.Dense(2, activation='softmax')(x)
+
+    model = tf.keras.Model(input_tensor, output_tensor)
+
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                 optimizer=keras.optimizers.Adam(lr = lr),
+                 metrics=['accuracy'])
+
+
+    # In[ ]:
+
+
+    model_filename = '/home/lauogden/gunshot_cnn_model.pkl' 
+
+    # add callbacks to stop early if it stops improving
+
+    callbacks = [
+        EarlyStopping(monitor='val_acc',
+                      patience=10,
+                      verbose=1,
+                      mode='auto'),
+
+        ModelCheckpoint(model_filename, monitor='val_acc',
+                        verbose=1,
+                        save_best_only=True,
+                        mode='auto'),
+    ]
+
+
+    # In[15]:
+
+
+    model.summary()
+
+
+    # In[16]:
+
+
+    #model.load("/home/lauogden/gunshot_cnn_model.h5")
+
+    #FIT IT
+    History = model.fit(train_wav, train_label, 
+              validation_data=[test_wav, test_label],
+              epochs=50,
+              callbacks=model_callbacks,
+              verbose=1,
+              batch_size=batch_size,
+              shuffle=True)
+
+    model.save("/home/lauogden/gunshot_cnn_model.h5")
+
+
+    # In[26]:
+
+
+    #incorrectly predicted data samples
+    Y_test_pred = model.predict(test_wav)
+    y_predicted_classes_test = y_test_pred.argmax(axis=-1)
+    y_actual_classes_test= test_label.argmax(axis=-1)
+    wrong_examples = np.nonzero(y_predicted_classes_test != y_actual_classes_test)
+    print("Wrong examples: ")
+    print(wrong_examples)
 
