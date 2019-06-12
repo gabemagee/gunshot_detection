bbe121fdafae4043e117297fd16a97df02abea7b
Gabe Magee <glma2016@pomona.edu>
2019-06-12 12:35:51 -0400

    update

---
+++ python/Gabe/cnn_with_cache.py
@@ -0,0 +1,35 @@
+import numpy as np
+from scipy.fftpack import fft
+from scipy import signal
+import librosa
+
+# Dimension Reduction
+from sklearn.decomposition import PCA
+
+# Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+import IPython.display as ipd
+import librosa.display
+import plotly.offline as py
+import plotly.graph_objs as go
+import plotly.tools as tls
+
+# Data Pre-processing
+import pandas as pd
+from sklearn.model_selection import KFold
+import soundfile
+
+# Deep Learning
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras import Input, layers
+from tensorflow.keras import backend as K
+
+from tensorflow.python.client import device_lib
+
+print(os.getcwd())
+cache_location = ""
+sample_file = cache_location+"/gunshot_sound_samples.npy"
+label_file = cache_location+"/gunshot_sound_labels.npy"
