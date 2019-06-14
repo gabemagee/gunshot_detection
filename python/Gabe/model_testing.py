9b66790d7e0ced60f23f25033292a3b98cb172ba
Gabe Magee <glma2016@pomona.edu>
2019-06-14 12:39:44 -0400

    ftp

--- python/Gabe/model_testing.py
+++ python/Gabe/model_testing.py
@@ -70,13 +70,16 @@ samples = "/home/gamagee/workspace/gunshot_detection/REU_Data/gunshot_augmented_
 model = keras.models.load_model(model_path,custom_objects={'auc':auc})
 model.summary()
 
+label_np = keras.utils.to_categorical(label_np, 2)
 
 label_np = np.array(np.load(labels))
+label_np = np.array(keras.utils.to_categorical(label_np, 2))
 sample_np = np.array(np.load(samples)).reshape(-1, sampling_rate_per_two_seconds, 1)
 a = model.predict(sample_np)
 print(a.shape)
 print(label_np.shape)
 a = np.argmax(a,axis=1)
+print(a.shape)
 b = np.argmax(label_np,axis=1)
 print(a)
 print(b)
