7fd6725ef9533146b35809e76cd61ab5ea2de229
Gabe Magee <glma2016@pomona.edu>
2019-06-14 12:34:00 -0400

    ftp

--- python/Gabe/model_testing.py
+++ python/Gabe/model_testing.py
@@ -73,8 +73,7 @@ model.summary()
 
 label_np = np.array(np.load(labels))
 sample_np = np.array(np.load(samples)).reshape(-1, sampling_rate_per_two_seconds, 1)
-print(sample_np.shape)
-a = model.predict(samples)
+a = model.predict(sample_np)
 
 print(a)
 
