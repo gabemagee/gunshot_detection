550825a248c2462ffb06869134dd3f532b416909
Gabe Magee <glma2016@pomona.edu>
2019-06-19 16:25:04 -0400

    a

--- python/Gabe/spectrogram_gabe.py
+++ python/Gabe/spectrogram_gabe.py
@@ -154,13 +154,13 @@ for file in os.listdir(sample_directory):
     sample,sr = librosa.load(sample_directory+file)
     if len(sample) <= sample_rate_per_two_seconds:
         number_of_missing_frames = sample_rate_per_two_seconds - len(sample)
-        padded_sample = np.array(sample.tolist() + [0 for i in range(number_of_missing_hertz)])
+        padded_sample = np.array(sample.tolist() + [0 for i in range(number_of_missing_frames)])
         label = d[file]["label"]
         samples.append(padded_sample)
         labels.append(label)
     else:
         number_of_missing_frames = len(sample) % sample_rate_per_two_seconds
-        sample = np.array(sample.tolist() + [0 for i in range(number_of_missing_hertz)])
+        sample = np.array(sample.tolist() + [0 for i in range(number_of_missing_frames)])
         for i in range(0, sample.size - sample_rate_per_two_seconds, sample_rate_per_two_seconds):
             sample_slice = sample[i : i + sample_rate_per_two_seconds]
             label = d[file]["label"]
