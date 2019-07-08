3046630fbc06ae5f3c696ee6f0fc3f9fa7a3d25f
Alex Morehead <alex.morehead@gmail.com>
2019-07-08 11:42:16 -0400

    Updated Model Inference Pipeline for Callbacks and Multithreading

--- raspberry_pi/pyaudio_callback_test.py
+++ raspberry_pi/pyaudio_callback_test.py
@@ -20,21 +20,19 @@ def callback(in_data, frame_count, time_info, status):
     sound_data = np.append(sound_data, data)
     print("i'th iteration:", i, len(data))
     i += 1
-    if len(sound_data) > 88200:
+    if len(sound_data) >= 88200:
         callback_queue.put(sound_data)
-        sound_data = np.zeros(88200, dtype = "int16")
-        return (data, pyaudio.paComplete)
+        sound_data = np.zeros(0, dtype = "int16")
+        i = 1
     return (data, pyaudio.paContinue)
 
-stream = pa.open(format = pyaudio.paInt16, input_device_index = 5, channels = 1, rate = 44100, input = True, stream_callback = callback)
+stream = pa.open(format = pyaudio.paInt16, input_device_index = 6, channels = 1, rate = 44100, input = True, frames_per_buffer = 4410, stream_callback = callback)
 
 stream.start_stream()
 
-while stream.is_active():
-    time.sleep(0.1)
-
-two_second_sample = callback_queue.get()
-print(two_second_sample.shape)
+while True:
+    two_second_sample = callback_queue.get()
+    print(two_second_sample.shape)
 
 stream.stop_stream()
 stream.close()
