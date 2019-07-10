1cde808e2eecb64428cd2e8bc88d1f672b3cece2
Gabe Magee <glma2016@pomona.edu>
2019-07-10 14:49:18 -0400

    a

--- python/Gabe/converter_tflite.py
+++ python/Gabe/converter_tflite.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import os
 
-saved_model_dir = os.getcwd() + "/raspberry_pi/models/new_spectrogram_model.h5"
+saved_model_dir = "/home/gamagee/workspace/gunshot_detection/raspberry_pi/models/new_spectrogram_model.h5"
 
 converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
 tflite_model = converter.convert()
