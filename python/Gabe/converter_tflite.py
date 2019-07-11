378f7e0d1ef1a266054df49cf469cb5708cab681
Gabe Magee <glma2016@pomona.edu>
2019-07-11 11:37:31 -0400

    models

--- python/Gabe/converter_tflite.py
+++ python/Gabe/converter_tflite.py
@@ -8,8 +8,8 @@ def auc(y_true, y_pred):
     return auc
 
 
-saved_model_dir = os.getcwd()+"/raspberry_pi/models/new_spectrogram_model.h5"
-converter = tf.lite.TFLiteConverter.from_keras_model_file(saved_model_dir,custom_objects={"auc":auc})
+saved_model_dir = os.getcwd()+"/raspberry_pi/models/spectro_model_no_variables.h5"
+converter = tf.lite.TFLiteConverter.from_keras_model_file(saved_model_dir)
 #converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
 tflite_model = converter.convert()
 open("converted_model.tflite", "wb").write(tflite_model)
