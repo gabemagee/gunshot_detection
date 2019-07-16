0fc8b1ada7ef1e4b180201dbc978652d70f26fb2
Gabe Magee <glma2016@pomona.edu>
2019-07-16 14:30:58 -0400

    testing

--- python/Gabe/model_testing_v2.py
+++ python/Gabe/model_testing_v2.py
@@ -81,7 +81,7 @@ rrr = list(train_test_indexes)
 print(len(rrr))
 l = []
 while len(l)<int(n*TESTING_RATIO/2):
-    i = np.random.choice(len(l),1)
+    i = np.random.choice(len(rrr),1)
     l.append(rrr.pop(i))
 
 #training
