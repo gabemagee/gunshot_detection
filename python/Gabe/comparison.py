b028d21f9d24745872cc31a7c3228a51b099f176
Gabe Magee <glma2016@pomona.edu>
2019-07-17 13:03:06 -0400

    h

--- python/Gabe/comparison.py
+++ python/Gabe/comparison.py
@@ -256,24 +256,24 @@ for i in range(len(validation_wav)):
     d = {}
 
     #CNN_2D_Model_keras
-    #x_1 = make_spectrogram(x).reshape((-1, 128, 87, 1))
-    x_1 = audio_to_melspectrogram(x).reshape((-1,128,64,1))
-    print(x_1.shape)
-    print(CNN_2D_Model_keras.layers[0].input_shape[0])
+    x_1 = make_spectrogram(x).reshape((-1, 128, 87, 1))
+    #x_1 = audio_to_melspectrogram(x).reshape((-1,128,64,1))
+    print("input shape",x_1.shape)
+    print("model input shape",CNN_2D_Model_keras.layers[0].input_shape[0])
     output = CNN_2D_Model_keras.predict(x_1)[0][1]
     print("CNN_2D_Model_keras",y,output)
 
     #CNN_1D_Model_keras
     x_1 = x
-    print(x_1.shape)
-    print(CNN_1D_Model_keras.layers[0].input_shape[0])
+    print("input shape",x_1.shape)
+    print("model input shape",CNN_1D_Model_keras.layers[0].input_shape[0])
     output = CNN_1D_Model_keras.predict(x_1)[0][1]
     print("CNN_1D_Model_keras",y,output)
 
     #gunshot_2d_spectrogram_model
     x_1 = x
-    print(x_1.shape)
-    print(gunshot_2d_spectrogram_model.layers[0].input_shape[0])
+    print("input shape",x_1.shape)
+    print("model input shape",gunshot_2d_spectrogram_model.layers[0].input_shape[0])
     output = gunshot_2d_spectrogram_model.predict(x_1)[0][1]
     print("gunshot_2d_spectrogram_model",y,output)
 
