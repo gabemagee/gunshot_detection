1416ae1a60d4d28aac6d098bf753fcab0541746b
Gabe Magee <glma2016@pomona.edu>
2019-07-19 13:29:57 -0400

    a

--- python/Gabe/comparison.py
+++ python/Gabe/comparison.py
@@ -369,14 +369,6 @@ print(t.draw())
 
 
 
-with open('data.pickle', 'wb') as f:
-    # Pickle the 'data' dictionary using the highest protocol available.
-    pickle.dump(model_scores, f, pickle.HIGHEST_PROTOCOL)
-
-with open('data.pickle', 'rb') as f:
-    # The protocol version used is detected automatically, so we do not
-    # have to specify it.
-    model_scores = pickle.load(f)
 
 tbl = Texttable()
 table = []
