c7e9b387081c2e99541d53fc630100a247c0ebd0
unknown <rjhosler@iupui.edu>
2019-07-22 18:05:05 -0400

    Volta code for spectrogram model

--- python/Ryan/spectrogram.py
+++ python/Ryan/spectrogram.py
@@ -39,10 +39,10 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import tensorflow as tf
 from tensorflow.keras import Input, layers, backend as K
 from tensorflow.keras.models import load_model, Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Activation, BatchNormalization, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Activation
+from tensorflow.keras.layers import BatchNormalization, Flatten, multiply
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
@@ -78,8 +78,8 @@ print("Type of the spectrograms array:", samples.dtype)
 
 
 sample_weights = np.array(
-    [1 for normally_recorded_sample in range(len(samples) - 660)] + [20 for raspberry_pi_recorded_sample in range(660)])
-print("Shape of samples weights before splitting:", sample_weights.shape)
+    [1 for normally_recorded_sample in range(len(samples) - 660)] + [15 for raspberry_pi_recorded_sample in range(660)])
+#print("Shape of samples weights before splitting:", sample_weights.shape)
 
 # ## Restructuring the label data
 
@@ -96,19 +96,33 @@ labels = np.hstack((labels, 1 - labels))
 # In[ ]:
 
 
-print("Shape of samples array:", samples.shape)
-print("Shape of labels array:", labels.shape)
+#print("Shape of samples array:", samples.shape)
+#print("Shape of labels array:", labels.shape)
 
 # ## Arranging the data
 
 # In[ ]:
 
-
+'''
 kf = KFold(n_splits=3, shuffle=True)
 for train_index, test_index in kf.split(samples):
     train_wav, test_wav = samples[train_index], samples[test_index]
     train_label, test_label = labels[train_index], labels[test_index]
     train_weights, test_weights = sample_weights[train_index], sample_weights[test_index]
+'''
+
+all_index = np.arange(len(samples))
+train_index = np.load("training_set_indexes.npy")
+test_index = np.load("testing_set_indexes.npy")
+valid_index = np.delete(all_index, list(train_index) + list(test_index))
+
+print(train_index)
+print(test_index)
+print(valid_index)
+
+train_wav, test_wav, valid_wav = samples[train_index], samples[test_index], samples[valid_index]
+train_label, test_label, valid_label = labels[train_index], labels[test_index], labels[valid_index]
+train_weights, test_weights, valid_weights = sample_weights[train_index], sample_weights[test_index], sample_weights[valid_index]
 
 
 # # Model
@@ -130,77 +144,73 @@ def auc(y_true, y_pred):
 # In[ ]:
 
 
-number_of_epochs = 100
+number_of_epochs = 50
 batch_size = 32
-optimizer = Adam(lr=0.001, decay=0.001 / 100)
-input_tensor = Input(shape=(128, 64))
-
-# In[ ]:
-
-
-""" Step 1: Instantiate a sequential model """
-
-model = Sequential()
+optimizer = Adam(lr=0.0009)
+input_tensor = Input(shape=(128, 64, 1))
+window = (4, 4)
 
 """ Step 2: Create the input and hidden layers """
 
 # First Layer
-model.add(Conv2D(32, (3, 3), padding="same", input_shape=(128, 64, 1)))
-model.add(Activation("relu"))
-model.add(BatchNormalization(axis=-1))
-model.add(MaxPooling2D(pool_size=(3, 3)))
-model.add(Dropout(0.25))
+x = Conv2D(32, window, padding="same", activation="relu")(input_tensor)
+x = BatchNormalization(axis=-1)(x)
+x = MaxPooling2D(pool_size=(4, 2))(x)
+x = Dropout(0.25)(x)
 
 # Second Layer: (CONV => RELU) * 2 => POOL
-model.add(Conv2D(64, (3, 3), padding="same"))
-model.add(Activation("relu"))
-model.add(BatchNormalization(axis=-1))
-model.add(Conv2D(64, (3, 3), padding="same"))
-model.add(Activation("relu"))
-model.add(BatchNormalization(axis=-1))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+x = Conv2D(64, window, activation="relu", padding="same")(x)
+x = BatchNormalization(axis=-1)(x)
+x = Conv2D(64, window, activation="relu", padding="same")(x)
+x = BatchNormalization(axis=-1)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+x = Dropout(0.25)(x)
 
 # Third Layer: (CONV => RELU) * 2 => POOL
-model.add(Conv2D(128, (3, 3), padding="same"))
-model.add(Activation("relu"))
-model.add(BatchNormalization(axis=-1))
-model.add(Conv2D(128, (3, 3), padding="same"))
-model.add(Activation("relu"))
-model.add(BatchNormalization(axis=-1))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+x = Conv2D(128, window, activation="relu", padding="same")(x)
+x = BatchNormalization(axis=-1)(x)
+x = Conv2D(128, window, activation="relu", padding="same")(x)
+x = BatchNormalization(axis=-1)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+x = Dropout(0.25)(x)
 
 '''
 # Fourth Layer: (CONV => RELU) * 2 => POOL
-model.add(Conv2D(256, (3, 3), padding="same"))
-model.add(Activation("relu"))
-model.add(BatchNormalization(axis=-1))
-model.add(Conv2D(256, (3, 3), padding="same"))
-model.add(Activation("relu"))
-model.add(BatchNormalization(axis=-1))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+x = Conv2D(256, window, activation="relu", padding="same")(x)
+x = BatchNormalization(axis=-1)(x)
+x = Conv2D(256, window, activation="relu", padding="same")(x)
+x = BatchNormalization(axis=-1)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+x = Dropout(0.25)(x)
 '''
 
 """ Step 3: Flatten the layers """
 
-model.add(Flatten())
+x = Flatten()(x)
 
 """ Step 4: Fully-connect the layers """
 
-model.add(Dense(1024))
-model.add(Activation("relu"))
-model.add(BatchNormalization())
-model.add(Dropout(0.5))  # Increasing dropout here to prevent overfitting
+x = Dense(1024, activation="relu")(x)
+x = BatchNormalization()(x)
+x = Dropout(0.5)(x)  # Increasing dropout here to prevent overfitting
 
-model.add(Dense(2))
-model.add(Activation("softmax"))
+'''
+#Attention layer
+attention_probs = Dense(1024, activation='softmax', name='attention_vec')(x)
+attention_probs = Dropout(0.5)(attention_probs)
+attention_mul = multiply([x, attention_probs], name='attention_mul')
+attention_mul = Dense(256)(attention_mul)
+'''
+
+x = Dense(256, activation="relu")(x) 
+x = BatchNormalization()(x)
+x = Dropout(0.5)(x)  # Increasing dropout here to prevent overfitting
+
+output_tensor = layers.Dense(2, activation="softmax")(x)
 
 """ Step 5: Compile the model """
-
+model = tf.keras.Model(input_tensor, output_tensor)
 model.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=[auc, "accuracy"])
-
 # ## Configuring model properties
 
 # In[ ]:
@@ -217,10 +227,14 @@ model_callbacks = [
     ModelCheckpoint(model_filename, monitor="val_acc",
                     verbose=1,
                     save_best_only=True,
-                    mode="max")
+                    mode="max"),
+
+    ReduceLROnPlateau(monitor='val_loss', 
+		      patience=6, 
+                      factor=0.6, 
+                      min_lr=1e-8)
 ]
 
-# ### Debugging of the model's architecture (optional)
 
 # In[ ]:
 
@@ -234,33 +248,33 @@ print(model.summary())
 
 History = model.fit(train_wav, train_label,
                     validation_data=[test_wav, test_label],
-                    epochs=50,
+                    epochs=100,
                     callbacks=model_callbacks,
                     verbose=1,
                     batch_size=batch_size,
                     sample_weight=train_weights,
                     shuffle=True)
 
-model.save(BASE_DIRECTORY + "gunshot_2d_spectrogram_model.h5")
+model.save(BASE_DIRECTORY + "CurrentModels/RYAN_smaller_spectrogram_model.h5")
 
-# ### Debugging of incorrectly-labeled examples (optional)
-
-# In[ ]:
-
-
-y_test_pred = model.predict(test_wav)
-y_predicted_classes_test = y_test_pred.argmax(axis=-1)
-y_actual_classes_test = test_label.argmax(axis=-1)
-wrong_examples = np.nonzero(y_predicted_classes_test != y_actual_classes_test)
-print(wrong_examples)
 
 # ## Converting model to TensorFlow Lite format
-
-# In[ ]:
-
-
+'''
 model_name = BASE_DIRECTORY + "gunshot_2d_spectrogram_model"
 converter = tf.lite.TFLiteConverter.from_keras_model_file(model_name + ".h5", custom_objects={"auc": auc})
 converter.post_training_quantize = True
 tflite_model = converter.convert()
 open(model_name + ".tflite", "wb").write(tflite_model)
+'''
+# ### Debugging of incorrectly-labeled examples (optional)
+
+# In[ ]:
+
+model.load_weights(BASE_DIRECTORY + "CurrentModels/RYAN_smaller_spectrogram_model.h5")
+
+y_test_pred = model.predict(valid_wav)
+y_predicted_classes_test = y_test_pred.argmax(axis=-1)
+y_actual_classes_test = valid_label.argmax(axis=-1)
+wrong_examples = np.nonzero(y_predicted_classes_test != y_actual_classes_test)
+print(wrong_examples)
+print(100 - (len(wrong_examples[0]) / len(valid_wav)) * 100)
