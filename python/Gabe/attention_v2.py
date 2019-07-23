7118252fddf28df1935205d5cb90ef98e7512826
Gabe Magee <glma2016@pomona.edu>
2019-07-23 13:43:09 -0400

    new attention model

--- python/Gabe/attention_v2.py
+++ python/Gabe/attention_v2.py
@@ -63,4 +63,4 @@ model.summary()
 base_directory = "/home/gamagee/workspace/gunshot_detection/test_train/"
 X_train, X_test = np.load(base_directory+"X_train.npy").reshape((16294, 128, 128, 1)),np.load(base_directory+"X_test.npy").reshape((16294, 128, 128, 1))
 Y_train, Y_test = np.load(base_directory+"y_train.npy"),np.load(base_directory+"y_test.npy")
-model_history = model.fit(X_train, Y_train,batch_size=20,validation_data=(X_test,Y_test),nb_epoch=50)
+model_history = model.fit(X_train, Y_train,batch_size=150,validation_data=(X_test,Y_test),nb_epoch=50)
